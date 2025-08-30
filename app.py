4b0171cf2766dff566dffa55eb7846f67dcb955e
Joar von Arndt <joarxpablo@protonmail.com>
2025-08-30 17:32:44 +0200

    Changes the root to /diplomacy

--- app.py
+++ app.py
@@ -52,11 +52,11 @@ def delete_user_message(user):
         with open('data.json', 'w') as f:
             json.dump(messages, f, indent=4)
 
-@app.route('/')
+@app.route('/diplomacy/')
 def home():
     return render_template('index.html')
             
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/diplomacy/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -69,7 +69,7 @@ def login():
             return 'Invalid credentials', 403
     return render_template('login.html')
 
-@app.route('/message', methods=['GET', 'POST'])
+@app.route('/diplomacy/message', methods=['GET', 'POST'])
 def message():
     if 'user' not in session:
         return redirect(url_for('login'))
@@ -82,7 +82,7 @@ def message():
     user_message = load_user_message(user)
     return render_template('message.html', current_user=user, user_message=user_message)
 
-@app.route('/delete', methods=['POST'])
+@app.route('/diplomacy/delete', methods=['POST'])
 def delete():
     if 'user' not in session:
         return redirect(url_for('login'))
@@ -90,12 +90,12 @@ def delete():
     delete_user_message(user)
     return redirect(url_for('message'))
 
-@app.route('/logout')
+@app.route('/diplomacy/logout')
 def logout():
     session.pop('user', None)
     return redirect(url_for('home'))
 
-@app.route('/menu')
+@app.route('/diplomacy/menu')
 def menu():
     archive_dir = './archive/'
     files = [f for f in os.listdir(archive_dir)]
@@ -110,7 +110,7 @@ def rewrite_static_paths(html_content):
     html_content = re.sub(r'(href|src)="static/([^"]*)"', r'\1="/static/\2"', html_content)
     return html_content
 
-@app.route('/archive/<filename>')
+@app.route('/diplomacy/archive/<filename>')
 def view_file(filename):
     if filename.endswith('.html'):
         file_path = os.path.join('./archive', filename)
@@ -124,6 +124,6 @@ def view_file(filename):
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route('/assets/<path:filename>')
+@app.route('/diplomacy/assets/<path:filename>')
 def custom_assets(filename):
     return send_from_directory(os.path.join(app.root_path, 'assets'), filename)
