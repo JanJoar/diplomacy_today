3469a4a24b2f7996f31fd0e9cd7346286c58c3b2
Joar von Arndt <joarxpablo@vonarndt.se>
2025-09-15 17:52:50 +0200

    Adds blueprint as a dependency

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify, send_from_directory, render_template_string
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, send_from_directory, render_template_string, Blueprint
 import json
 import os
 import re
